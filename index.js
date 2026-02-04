6af327dd29deec429c5f874df9ba08a4871dcb77
kao678 <kaobas2536@gmail.com>
2026-02-05 00:56:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const express = require("express");
-const line = require("@line/bot-sdk");
-const bodyParser = require("body-parser");
-
-const config = require("./config");
-const { handleCommand } = require("./commands");
-
 const app = express();
-app.use(bodyParser.json());
 
-const client = new line.Client(config);
+app.use(express.json());
 
-app.post("/webhook", async (req, res) => {
-  const events = req.body.events;
-  for (let event of events) {
-    if (event.type === "message" && event.message.type === "text") {
-      await handleCommand(event, client);
-    }
-  }
+app.get("/", (req, res) => {
+  res.send("RENDER BOT IS ALIVE");
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Webhook OK");
   res.sendStatus(200);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("BOT RUNNING");
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
